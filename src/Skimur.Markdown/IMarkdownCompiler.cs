11984287cafbcc54fdca00dd5acd415fda04aeb0
Eric Hocking <ehock7229@outlook.com>
2020-04-08 12:49:31 -0400

    begin work on markdown stuff

---
+++ src/Skimur.Markdown/IMarkdownCompiler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Skimur.Markdown
+{
+    public interface IMarkdownCompiler : IDisposable
+    {
+        string Compile(string markdown);
+
+        string Compile(string markdown, out List<string> mentions);
+    }
+}
---
+++ src/Skimur.Markdown/Registrar.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Jering.Javascript.NodeJS;
+using Skimur.Common.Utils;
+
+namespace Skimur.Markdown
+{
+    public class Registrar : IRegistrar
+    {
+
+        public void Register(IServiceCollection services)
+        {
+            services.AddNodeJS();
+        }
+
+        public int Order { get { return 0; } }
+    }
+}
